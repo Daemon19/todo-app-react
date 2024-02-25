3b5962799f32dfa94ed1e1d3b251e8075b7ecfbd
Daemon19 <komanglangendria04@gmail.com>
2024-02-25 22:44:11 +0800

    Remove unused Multiplier component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useMemo, useState } from 'react'
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
@@ -30,7 +29,6 @@ function App() {
     <>
       <TodoList todos={todos} />
       <TodoForm onAddTodo={handleAddTodo} />
-      <Multiplier />
     </>
   )
 }
@@ -57,44 +55,6 @@ function TodoForm({ onAddTodo }) {
       <button type="submit">Add todo</button>
     </form>
   )
-}
-
-function Multiplier() {
-  const [number, setNumber] = useState(0)
-  const doubleNumber = useMemo(() => {
-    return slowFunction(number)
-  }, [number])
-
-  const [dark, setDark] = useState(false)
-  const themeStyles = useMemo(() => {
-    return {
-      backgroundColor: dark ? 'black' : 'white',
-      color: dark ? 'white' : 'black',
-    }
-  }, [dark])
-
-  useEffect(() => {
-    console.log('Theme changed!')
-  }, [themeStyles])
-
-  return (
-    <>
-      <br />
-      <input
-        type="number"
-        onChange={(e) => setNumber(e.target.value)}
-        value={number}
-      />
-      <br />
-      <button onClick={() => setDark(!dark)}>Change theme</button>
-      <p style={themeStyles}>{doubleNumber}</p>
-    </>
-  )
-}
-
-function slowFunction(a) {
-  for (let i = 0; i < 200000000; i++) {}
-  return a * 2
 }
 
 export default App
