8f58df73ccc579dca3de997d049c3abb53d157c6
Daemon19 <komanglangendria04@gmail.com>
2024-02-29 22:11:55 +0800

    fix: vertically uncenter title through line

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <div className="mx-auto p-5 pb-24 max-w-screen-sm flex flex-col gap-3">
-        <h1 className="relative w-fit mx-auto text-6xl font-bold text-center py-5 px-2 after:absolute after:content-[''] after:block after:h-1 after:w-full after:bg-black after:top-[58%] after:left-0">
+        <h1 className="relative w-fit mx-auto text-6xl font-bold text-center py-5 px-2 after:absolute after:h-1 after:w-full after:bg-black after:top-[50%] after:left-0">
           Todos
         </h1>
         <TodoList todos={todos} />
@@ -24,4 +24,3 @@ function App() {
 }
 
 export default App
-
