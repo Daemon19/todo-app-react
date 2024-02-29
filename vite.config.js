dd25b90515b0770d14c38a3add30b53c4995e18f
Daemon19 <komanglangendria04@gmail.com>
2024-02-29 21:43:57 +0800

    feat: read dev server port from env file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/todo-app-react/',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, import.meta.dirname)
+  return {
+    plugins: [react()],
+    base: '/todo-app-react/',
+    server: {
+      port: env.VITE_PORT,
+    },
+  }
 })
 
