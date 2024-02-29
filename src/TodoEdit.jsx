043b1b2f5d88d4c2b998396b5bdf2a9e690441f3
Daemon19 <komanglangendria04@gmail.com>
2024-02-29 22:20:36 +0800

    fix: todo edit form's overflow on small screen

--- src/TodoEdit.jsx
+++ src/TodoEdit.jsx
@@ -8,7 +8,10 @@ export function TodoEdit({ todo, onSave, onCancel }) {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex gap-2 w-full">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 w-full sm:flex-row"
+    >
       <input
         name="todo"
         type="text"
@@ -16,21 +19,23 @@ export function TodoEdit({ todo, onSave, onCancel }) {
         autoFocus
         autoComplete="off"
         defaultValue={todo.body}
-        className="block flex-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        className="block flex-1 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
       />
-      <button
-        type="button"
-        onClick={onCancel}
-        className="rounded-md bg-gray-100 px-3 py-1.5 text-sm leading-6 text-black shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300"
-      >
-        Cancel
-      </button>
-      <button
-        type="submit"
-        className="flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        <FaFloppyDisk /> Save
-      </button>
+      <div className="flex gap-2 justify-end">
+        <button
+          type="button"
+          onClick={onCancel}
+          className="rounded-md bg-gray-100 px-3 py-1.5 text-sm leading-6 text-black shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          <FaFloppyDisk /> Save
+        </button>
+      </div>
     </form>
   )
 }
