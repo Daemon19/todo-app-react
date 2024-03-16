7a96b0ac2c723b1bb5df7ab2feb513fd3e9e9312
Daemon <komanglangendria04@gmail.com>
2024-03-16 10:07:35 +0000

    feat: add loading spinner when loading todos

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,16 @@
 import { Todo } from './Todo'
+import { LoadingSpinner } from './components/ui/loading-spinner'
 import useTodos from './hooks/useTodos'
 
 export function TodoList() {
   const { data: todos, error, isLoading } = useTodos()
 
-  if (error) return <p>Failed to load todos.</p>
-  if (isLoading) return <p>Loading todos...</p>
+  if (error) {
+    return <p>Failed to load todos.</p>
+  }
+  if (isLoading) {
+    return <LoadingSpinner className="mx-auto size-16" />
+  }
 
   const sortedTodos = todos.toSorted((a, b) => Number(a.done) - Number(b.done))
   const todoElements = sortedTodos.map((todo) => (
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,8 +9,12 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     base: '/todo-app-react/',
     server: {
-      port: env.VITE_PORT,
+      port: env.VITE_PORT
     },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
   }
 })
-
